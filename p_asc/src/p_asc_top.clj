9db0bea67767a3478edb7a8d8fa0bd6367edfd4e
Magnus <magnus.ersdal@uib.no>
2021-12-07 17:09:42 +0100

    initial commit

---
+++ p_asc/src/p_asc_top.clj
@@ -0,0 +1,12 @@
+(ns sites.p-asc-top
+  (:require [hiccup.core :as hc]
+            [hiccup.page :refer [html5 include-css include-js]]))
+
+(def output-folder "../")
+
+(comment 
+  (defn serve []
+    )
+  (serve)
+  )
+
